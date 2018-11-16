68c85b4d1987a8e18ad1d1d9fd76ef8d7ca60d92
Garett Bass <garett.bass@king.com>
2018-11-15 17:54:27 -0800

    added SampleSubassetWithEnumValue to diagnose enum-related UI bug

---
+++ InspectInlineExamples/SampleSubassetWithEnumValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UnityExtensions.InspectInlineExamples
+{
+
+    // [CreateAssetMenu]
+    public class SampleSubassetWithEnumValue : SampleSubasset
+    {
+
+        public enum Enum
+        {
+            Foo,
+            Bar,
+            Baz,
+        }
+
+        public Enum enumValue;
+
+    }
+
+}
